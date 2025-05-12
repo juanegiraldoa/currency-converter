b947ab1430ed16e76cafec17d7e6382f19d5a498
Juan Giraldo <juanegiraldoa@proton.me>
2025-05-12 10:27:38 -0500

    Initial commit

---
+++ src/pages/api/currency/[id].ts
@@ -0,0 +1,30 @@
+import type { APIRoute } from "astro";
+
+const URL = "https://duckduckgo.com/js/spice/currency_convert/1/USD";
+
+export interface Currency {
+  timestamp: Date;
+}
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, locals }) => {
+  const currency = params.id;
+  if (!currency) return new Response();
+
+  const { CURRENCIES } = locals.runtime.env;
+  const currentString = (await CURRENCIES.get(currency)) || "{}";
+  const current = JSON.parse(currentString) as Currency;
+  const today = new Date();
+  const currentTime = new Date(current.timestamp);
+  if (today.getMinutes() !== currentTime.getMinutes()) {
+    const response = await fetch(`${URL}/${currency}`);
+    const data = await response.json();
+    const { to, timestamp } = data as any;
+    const [quote] = to;
+    CURRENCIES.put(currency, JSON.stringify({ ...quote, timestamp }));
+  }
+
+  return new Response(JSON.stringify({}));
+  // return new Response(JSON.stringify({ ...quote, timestamp }));
+};

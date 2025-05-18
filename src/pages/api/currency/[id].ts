164da7cbe75cf2ec345723c5a8411ecb90ae500c
Juan Giraldo <juanegiraldoa@proton.me>
2025-05-18 15:05:22 -0500

    Add cloudflare KV

--- src/pages/api/currency/[id].ts
+++ src/pages/api/currency/[id].ts
@@ -10,22 +10,25 @@ export const prerender = false;
 
 export const GET: APIRoute = async ({ params, locals }) => {
   const currency = params.id;
-  if (!currency) return new Response();
+  if (!currency) return new Response("Send currency param");
 
   const { CURRENCIES } = locals.runtime.env;
   const currentString = (await CURRENCIES.get(currency)) || "{}";
-
   const current = JSON.parse(currentString) as Currency;
+
   const today = new Date();
   const currentTime = new Date(current.timestamp);
-  // if (today.getMinutes() !== currentTime.getMinutes()) {
-  const response = await fetch(`${URL}/${currency}`);
-  const data = await response.json();
-  const { to, timestamp } = data as any;
-  const [quote] = to;
-  CURRENCIES.put(currency, JSON.stringify({ ...quote, timestamp }));
-  // }
 
-  // return new Response(JSON.stringify({}));
-  return new Response(JSON.stringify({ ...quote, timestamp }));
+  console.log(current);
+
+  if (today.getMinutes() !== currentTime.getMinutes()) {
+    const response = await fetch(`${URL}/${currency}`);
+    const data = await response.json();
+    const { to, timestamp } = data as any;
+    const [quote] = to;
+    CURRENCIES.put(currency, JSON.stringify({ ...quote, timestamp }));
+    return new Response(JSON.stringify({ ...quote, timestamp }));
+  }
+
+  return new Response(JSON.stringify(current));
 };

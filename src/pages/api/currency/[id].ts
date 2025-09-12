b779dcd6078063a28ef33ad4bf18044593dde803
Juan Giraldo <juanegiraldoa@proton.me>
2025-09-12 15:28:05 -0500

    Add conversion

--- src/pages/api/currency/[id].ts
+++ src/pages/api/currency/[id].ts
@@ -1,32 +1,37 @@
 import type { APIRoute } from "astro";
 
-const URL = "https://duckduckgo.com/js/spice/currency_convert/1/USD";
+const BASE_URL = "https://duckduckgo.com/js/spice/currency_convert";
 
 export interface Currency {
-  timestamp: Date;
+	timestamp: Date;
 }
 
 export const prerender = false;
 
-export const GET: APIRoute = async ({ params, locals }) => {
-  const currency = params.id;
-  if (!currency) return new Response("Send currency param");
+export const GET: APIRoute = async ({ params, locals, url }) => {
+	const currency = params.id;
+	if (!currency) return new Response("Send currency param");
 
-  const { CURRENCIES } = locals.runtime.env;
-  const currentString = (await CURRENCIES.get(currency)) || "{}";
-  const current = JSON.parse(currentString) as Currency;
+	const searchParams = new URLSearchParams(url.search);
+	const amount = parseFloat(searchParams.get("amount") || "1");
 
-  const today = new Date();
-  const currentTime = new Date(current.timestamp);
+	const { CURRENCIES } = locals.runtime.env;
+	const currentString = (await CURRENCIES.get(currency)) || "{}";
+	const current = JSON.parse(currentString) as Currency;
 
-  if (today.getMinutes() !== currentTime.getMinutes()) {
-    const response = await fetch(`${URL}/${currency}`);
-    const data = await response.json();
-    const { to, timestamp } = data as any;
-    const [quote] = to;
-    CURRENCIES.put(currency, JSON.stringify({ ...quote, timestamp }));
-    return new Response(JSON.stringify({ ...quote, timestamp }));
-  }
+	const today = new Date();
+	const currentTime = new Date(current.timestamp);
 
-  return new Response(JSON.stringify(current));
+	if (today.getMinutes() !== currentTime.getMinutes()) {
+		const response = await fetch(`${BASE_URL}/${amount}/USD/${currency}`);
+		const data = await response.json();
+		const { to, timestamp } = data as any;
+		const [quote] = to;
+		const result = { ...quote, timestamp, amount };
+		CURRENCIES.put(currency, JSON.stringify(result));
+		return new Response(JSON.stringify(result));
+	}
+
+	const result = { ...current, amount };
+	return new Response(JSON.stringify(result));
 };
